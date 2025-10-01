9c1dcd3d9bddbf69f48eb5d04661a0d6f5ac994e
Vicker <vcalero2001@gmail.com>
2025-10-01 14:40:35 +0200

    Implementación de ejercicio 1
    Pasa el test correspondiente

--- Practica1.java
+++ Practica1.java
@@ -6,8 +6,27 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        List<Integer> listaElementos = new ArrayList<>();
+        while (it.hasNext()) {
+            listaElementos.add(it.next());
+        }
+
+        Set<Integer> resultado = new HashSet<>();
+
+        for (int i = 0; i < listaElementos.size(); i++) {
+            int num = listaElementos.get(i);
+            if (num == 0) {continue;}
+
+            for (int j = 0; j < listaElementos.size(); j++) {
+                if (i == j) {continue;}
+                int otroNum = listaElementos.get(j);
+                if (otroNum != 0 && num % otroNum == 0) {
+                    resultado.add(num);
+                    break;
+                }
+            }
+        }
+        return resultado;
     }
 
     //EJERCICIO2
